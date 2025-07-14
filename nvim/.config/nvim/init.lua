5caf51c3ddca2ce49be1182935b51f09dcee288c
Emil Due Rasmussen <emildr12@gmail.com>
2025-07-14 22:47:46 +0200

    Go error shortcut

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -137,6 +137,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Go error handling
+vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR>}<Esc>Oreturn err<Esc>')
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
